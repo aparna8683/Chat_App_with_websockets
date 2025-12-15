d1c682fea141d8826733ef9ca96ed2ae906373b4
Aparna Singh <aparnasingh868383@gmail.com>
2025-12-15 23:24:14 +0530

    Added Signup Form

--- frontend/src/Pages/SignUpPage.jsx
+++ frontend/src/Pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useAuth } from "../store/useAuthStore";
+import { MessageSquare, User } from "lucide-react";
 
 const SignUpPage = () => {
   const { authUser } = useAuth();
@@ -11,8 +12,53 @@ const SignUpPage = () => {
   });
   const { signup, isSignininUp } = useAuth();
   const validateForm = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-  return <div>SignUpPage</div>;
+  return (
+    <div>
+      SignUpPage
+      {/* //leftside */}
+      <div className="flex flex-col justify-center items-center py-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/* ??LOgo */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2"> Create Account </h1>
+              <p className="text-base-content/60">
+                Get started with your free account
+              </p>
+            </div>
+          </div>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Full Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0  pl-3 flex items-center pointer-events-none">
+                  <User className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="Aparna "
+                  value={formData.fullName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, fullName: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
